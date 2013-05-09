748b8ff7831b4b46c77443b69627461bdc5a22e4
Yuanwei <945652335@qq.com>
2013-05-09 11:20:21 +0800

    use epoll to realize

--- webServer/main.c
+++ webServer/main.c
@@ -2,9 +2,6 @@
 /*
  * head files
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <netinet/in.h>
 #include "webserver.h"
 
 /*
@@ -12,30 +9,60 @@
  */
 int main(int argc, char *argv[])
 {
-	struct sockaddr_in cin;
-	socklen_t csocklen = sizeof(cin);
-	int sfd, cfd;
-	pid_t pid;
+	struct sockaddr_in sin, cin;
+	int port;
+	int sfd, cfd, epfd;
+	struct epoll_event ev, events[10];
+	int nfds;
+	int m;
+	int csocklen;
+	char request_buf[10][MAX_REQUEST];
+	int command;
 	
 	init(&sfd);
+	
+	epfd = epoll_create(50);
+	ev.data.fd = sfd;
+	ev.events = EPOLLIN | EPOLLET;
+	epoll_ctl(epfd, EPOLL_CTL_ADD, sfd, &ev);
 
 	while(1)
 	{
-		cfd = accept(sfd, (struct sockaddr *)&cin, &csocklen);
-		printf("accept!\n");
+		nfds = epoll_wait(epfd, events, 10, 500);
 
-		/*It may result in zomble process.*/
-		pid = fork();
-		if(0 == pid)
+		for(m = 0; m < nfds; m++)
 		{
-			close(sfd);
-			doRequest(cfd);
-			close(cfd);
-			exit(0);
-		}
-		else
-		{
-			close(cfd);
+			if(events[m].data.fd == sfd)
+			{
+				cfd = accept(sfd, (struct sockaddr *)&cin, &csocklen);
+				printf("accept!\n");
+				ev.data.fd = cfd;
+				ev.events = EPOLLIN | EPOLLET;
+				epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &ev);
+			}
+			else if(events[m].events & EPOLLIN)
+			{
+				getRequest(events[m].data.fd, request_buf[m]);
+				ev.data.fd = events[m].data.fd;
+				ev.events = EPOLLOUT | EPOLLET;
+				epoll_ctl(epfd, EPOLL_CTL_MOD, events[m].data.fd, &ev);
+			}
+			else if(events[m].events & EPOLLOUT)
+			{
+				getCommand(request_buf[m], &command);
+				switch(command)
+				{
+				case GET:
+					doGet(events[m].data.fd, request_buf[m]);
+					break;
+				default:
+					status501(events[m].data.fd);
+					exit(-1);
+				}
+				ev.data.fd = events[m].data.fd;
+				ev.events = EPOLLIN | EPOLLET;
+				epoll_ctl(epfd, EPOLL_CTL_MOD, events[m].data.fd, &ev);
+			}
 		}
 	}
 
--- webServer/webserver.h
+++ webServer/webserver.h
@@ -1,6 +1,46 @@
 
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+/*
+ * head files
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+
+/*
+ * define micro
+ */
+#define PORT_STR_SIZE 6
+#define MAX_LINE 100
+#define MAX_REQUEST 2000
+#define MAX_RW 100
+#define MAX_COMMAND 20
+
+#define GET 1
+#define PUT 2
+
+#define ON 1
+#define OFF 0
+
+#define HOME_PATH "./webserver.config"
+
 /*
  * declare function
  */
 int init(int *sfd);
-int doRequest(int cfd);
+int getRequest(int cfd, char *request_buf);
+int getCommand(char *request_buf, int *command);
+int doGet(int cfd, char *request_buf);
+int status500(int cfd);
+int status501(int cfd);
+int status404(int cfd, int isLongConnect);
+
+#endif
